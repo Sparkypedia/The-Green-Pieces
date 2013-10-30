64027c6a6880b75e7cae3729c79ebda3e1f456be
Sparkypedia <alex_malcolm@hotmail.co.uk>
2013-10-30 21:04:21 +0000

    update

--- Alexs-Code/LCD_Test1.c
+++ Alexs-Code/LCD_Test1.c
@@ -1,24 +1,31 @@
 
 //**********************************************************
-/* NOTE: before running make sure SPI code is laoded with 
- > gpio load spi						
+/* 	NOTE: Before running > gpio load spi						
 					
- Module pin				Raspberry pin	wiringPI#	
+	Module pin								Raspberry pin	wiringPI#	
  
- 1 VDD                        1					
- 2 VSS                        6					
- 3 PS (low for serial)        6					
- 4 DB7/SID                    19			12		
- 5 DB6/SCLK                   23			14		
- 6-11 DB5-DB0                 N/C					
- 12 E_RD                      N/C			
- 13 RW_W                      N/C					
- 14 RS (0=ctrl mode, 1=data   5				9		
- 15 RES reset input           3				8		
- 16 CS                        6					
- 17 Led+      				  requires external bias 				
- 18 Led-                      6							  */
-//**********************************************************
+	1 VDD                      					   1					
+	2 VSS                      					   6					
+	3 PS (Parallel/Serial Select)(low for serial)  6					
+	4 DB7/SID                   				  19			2		
+	5 DB6/SCLK                   				  23			4		
+	6-11 DB5-DB0                				  N/C					
+	12 E_RD                      				  N/C			
+	13 RW_W                      				  N/C					
+	14 RS (Register Select)(0=ctrl mode, 1=data)   5			9		
+	15 RESET Input Pin	         				   3			8		
+	16 CS (Chip Select-device active when low)     6					
+	17 Backlight Led+ (requires external bias) 				
+	18 Backlight Led-                      		   6	
+							  
+//***************Function Descriptions**********************
+								  											  
+	digitalwrite(pin,value)								  
+	pinMode(pin,direction)								  
+	wiringPiSPIDataRW (channel, data, int length)
+	wiringPiSPISetup (channel,speed)					  */
+
+//***********************Librarys***************************
 
 #include <wiringPi.h>
 #include <wiringPiSPI.h>
@@ -28,61 +35,87 @@
 #include <errno.h>
 #include <string.h>
 #include "font5x7int.h"
-
-//**********************************************************
-/*	Function Descriptions								  
-														  
-	digitalwrite(pin,value)								  
-	pinMode(pin,direction)								  
-	wiringPiSPIDataRW (channel, data, int length)
-	wiringPiSPISetup (channel,speed)
-														  */
-//**********************************************************
-
-int main (void)
+	
+//*****************Prototype Functions**********************
+	
+	void InitPi(void);
+	void write_cmd(int i);
+	void gotoxy(int x,int y);
+	void write_str(char *Str);
+	
+//***************Global Variables/Constants*****************
+	
+	uint8_t spiData [10];
+	
+//*******************Main Functions*************************
+	
+int main(void)
 {
-uint8_t spiData [10] ;
 
-// set interface speed
-if (wiringPiSPISetup (0,100000) < 0)
+	InitPi();								// Initilise the Pi
+	write_cmd(175);							// display on
+	write_cmd(167);							// reverse
+	write_cmd(226);							// reset display
+	write_cmd(47); 							// power control
+	write_cmd(179); 						// set row (180-row)
+	write_cmd(163); 						// LCD bias select
+	delayMicroseconds (100);
+
+	//Write text/graphics to display
+
+	gotoxy(179,16);							//row 1, column 0
+	write_str("Green Piece");
+	
+	gotoxy(178,16);							//row 2, column 0
+	write_str(" ");
+
+	gotoxy(177,16);							//row 3, column 0
+	write_str("Mystery Machines");
+
+	gotoxy(176,16);							//row 4, column 0
+	write_str(" ");
+
+	return 0;
+}
+
+void InitPi(void)	// Initilises the Pi & LCD (inc. pins, speed etc)
 	{
-	fprintf (stderr, "Unable to open SPI device 0: %s\n", strerror (errno)) ;
-	exit (1) ;
+	if (wiringPiSPISetup (0,100000) < 0)
+		{
+		fprintf (stderr, "Unable to open SPI device 0: %s\n", strerror (errno)) ;
+		exit (1) ;
+		}
+	wiringPiSetupSys();
+	wiringPiSetup();
+	
+	pinMode(8, OUTPUT); 				// reset pin = output
+	pinMode(9, OUTPUT); 				// cmd pin = output
+
+	digitalWrite(8,0);					// resets the LCD
+	delayMicroseconds(200);
+	digitalWrite(8,1);					// initilises the LCD
 	}
 
-wiringPiSetupSys() ;
-wiringPiSetup() ;
-
-/* setup output ports */
-pinMode(8, OUTPUT); /* reset */
-pinMode(9, OUTPUT); /* rs */
-
-/* reset LCD display */
-digitalWrite(8,0);
-delayMicroseconds (100) ;
-
-digitalWrite(8,1);
-
-void write_cmd(int i)
+void write_cmd(int i)		// Sends command to LCD Controller
 	{
-	digitalWrite(9,0); 					// set rs bit for control mode
+	digitalWrite(9,0); 					// Activate control mode
 	spiData[0]=i;
 	wiringPiSPIDataRW (0, spiData, 1) ;
 	}
 	
-void gotoxy(int x,int y)
-{
-	digitalWrite(9,0); 					// set rs bit for control mode
+void gotoxy(int x,int y)	// Sends x & y co-ordinates to LCD
+	{
+	digitalWrite(9,0); 					// Activate control mode
 	spiData[0]=x;
 	wiringPiSPIDataRW (0, spiData, 1) ;
 	spiData[0]=y;
 	wiringPiSPIDataRW (0, spiData, 1) ;
-}
+	}
 	
-void write_char(char Char)
+void write_char(char Char)  // 
 	{
 	int c;
-	digitalWrite(9,1); 					// set rs bit for control mode
+	digitalWrite(9,1); 					// Activate control mode
 	for(c=0;c<=5;c++)					// font set is 5 bits wide
 		{
 		spiData[c]=(font5x7int[Char*5+c]);
@@ -99,32 +132,7 @@ void write_str(char *Str)
 		write_char(Str[c]);
 		}
 	while(c<23) {
-		write_char(' '); 				// fill with white space
+		write_char(' '); 				// pad with spaces
 		c++;
 		}
 	}
-
-write_cmd(175);							//display on
-write_cmd(167);							// reverse
-write_cmd(226);							// reset display
-write_cmd(47); 							// power control
-write_cmd(179); 						// set row (180-row)
-write_cmd(163); 						// LCD bias select
-delayMicroseconds (100) ;
-
-//Write text/graphics to display
-
-gotoxy(179,16);							//row 1, column 0
-write_str("Green Pieces");
-
-gotoxy(178,16);							//row 2, column 0
-write_str(" ");
-
-gotoxy(177,16);							//row 3, column 0
-write_str("Mystery Machine");
-
-gotoxy(176,16);							//row 4, column 0
-write_str(" ");
-
-return 0 ;
-}
